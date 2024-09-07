f9662c80d1cb7fcbc63d370d9ebbea64f95402c4
CervantesSanti2005 <SantiagoCervantes@ciencias.unam.mx>
2024-09-06 20:40:11 -0600

    Añadiendo el protocolo al servidor

---
+++ Proyecto1/Protocolo.cs
@@ -0,0 +1,40 @@
+namespace Servidor{
+    public static class Protocolo{
+        public enum MessageType{
+
+            IDENTIFY,
+            STATUS,
+            USERS,
+            TEXT,
+            PUBLIC_TEXT,
+            NEW_USER,
+            NEW_STATUS,
+            USER_LIST,
+            TEXT_FROM,
+            PUBLIC_TEXT_FROM,
+            RESPONSE
+        }
+        public enum UserStatus{
+
+            ACTIVE,
+            AWAY,
+            BUSY,
+
+        }
+        public enum OperationResult{
+            SUCCESS,
+            USER_ALREADY_EXISTS,
+            NO_SUCH_USER,
+            INVALID
+        }
+        public enum OperationType{
+
+            IDENTIFY,
+            STATUS,
+            USERS,
+            TEXT,
+            PUBLIC_TEXT,
+            INVALID,
+        }
+    }
+}
