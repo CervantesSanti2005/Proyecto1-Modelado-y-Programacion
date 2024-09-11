52e0d1444b45b55612a041841509ec86c82a28aa
CervantesSanti2005 <SantiagoCervantes@ciencias.unam.mx>
2024-09-11 00:45:49 -0600

    Separando mi codigo en más clases, clase estado que define la informacion de cada cliente que se conecta

---
+++ Proyecto1/Estado.cs
@@ -0,0 +1,18 @@
+using System.Net.Sockets;
+using System.Text;
+
+namespace Servidor{
+
+    public class Estado{
+
+        public Socket socketCliente = null;
+        public const int TamanoBuffer = 1024;
+        public byte[] buffer = new byte[TamanoBuffer];
+        public StringBuilder sb = new StringBuilder();
+        public int numeroCliente;
+        public string username = "";
+        public bool identificado = false;
+        public string status = "ACTIVE";
+    }
+
+}
